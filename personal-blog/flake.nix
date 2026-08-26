261aed702a2ac71b5bb582113e514d0b751ef0ac
Andrew Mortimer <andrcmdr@protonmail.com>
2026-08-26 19:23:13 -0400

    chore: update submodule pointers and format changes
    ## Executive Summary
    
    Updated cmdr, cdc, and personal-blog submodule pointers to latest commits. Applied formatting updates to personal-blog/flake.nix.
    
    ## Changes
    
    1. cmdr: cef9e66 - Added comprehensive verbose comments to flake.nix
    2. unimart-employee-handbooks/cdc: d911def - Auto-synced commit log from cmdr changes
    3. personal-blog/flake.nix: Applied nixfmt formatting (output parameter restructuring)
    
    These are routine sync commits from post-commit hooks and submodule updates.
    
    Signed-off-by: Andrew Mortimer <andrcmdr@protonmail.com>

--- personal-blog/flake.nix
+++ personal-blog/flake.nix
@@ -6,8 +6,14 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, flake-utils, nixpkgs }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      flake-utils,
+      nixpkgs,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
       in
